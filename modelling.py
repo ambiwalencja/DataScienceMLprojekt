13a9a8e5fbf96b845d0cac8b59f97f9e6df0df4c
ambiwalencja <helena.a.sokolowska@gmail.com>
2023-05-28 12:54:45 +0200

    fixes in compare_results function and making standarize_dataset function static

--- modelling.py
+++ modelling.py
@@ -36,28 +36,37 @@ results = pd.DataFrame()
 
 
 def compare_results(method, auroc_mean):
-    temp_results = pd.DataFrame({'Method': [method], 'ROC AUC score': round(auroc_mean, 3)}, index=['2'])
-    return pd.concat([results, temp_results])
+    global results # trzeba to dodać, żeby interpretter wiedział, że results to jest zmienna globalna i żeby nie
+                    # próbował sie od niej odwołac jako do lokalneji żeby nie wyrzucał błędu
+                    # "UnboundLocalError: local variable 'results' referenced before assignment
+
+    temp_results = pd.DataFrame({'Method': method,
+                                 'ROC AUC score': round(auroc_mean, 3)},
+                                index=[len(results.index) + 1])
+    results = pd.concat([results, temp_results])
+    return results
+
+
+def standarize_dataset(array_like_object):
+    scaled_array = scaler.fit_transform(array_like_object)
+    return scaled_array
 
 
 class Model:
-    def __init__(self, x, y):
+    def __init__(self, x, y, model_class):
         self.auroc_mean = 0
         self.list_auroc_stratified = []
         self.df_x = x
         self.df_y = y
+        self.model = model_class
 
-    def standarize_dataset(self, array_like_object):
-        scaled_array = scaler.fit_transform(array_like_object)
-        return scaled_array
 
     def print_results(self):
         print('List of AUROC scores:', self.list_auroc_stratified)
         self.auroc_mean = np.mean(self.list_auroc_stratified)
         print('\nMean AUROC:', round(self.auroc_mean * 100, 3), '%')
 
-    def train_model(self, model_class):
-        model = model_class
+    def train_model(self):
         self.list_auroc_stratified = []
         array_x = self.df_x.to_numpy()
         array_y = self.df_y.to_numpy()
@@ -65,49 +74,19 @@ class Model:
             x_train_fold, x_test_fold = array_x[train_index], array_x[test_index]
             y_train_fold, y_test_fold = array_y[train_index], array_y[test_index]
 
-            scaled_x_array_train_fold = self.standarize_dataset(x_train_fold)
-            scaled_x_array_test_fold = self.standarize_dataset(x_test_fold)
+            scaled_x_array_train_fold = standarize_dataset(x_train_fold)
+            scaled_x_array_test_fold = standarize_dataset(x_test_fold)
 
-            model.fit(scaled_x_array_train_fold, np.ravel(y_train_fold))
+            self.model.fit(scaled_x_array_train_fold, np.ravel(y_train_fold))
             self.list_auroc_stratified.append(
-                roc_auc_score(y_test_fold, model.predict_proba(scaled_x_array_test_fold)[:, 1]))
+                roc_auc_score(y_test_fold, self.model.predict_proba(scaled_x_array_test_fold)[:, 1]))
         self.print_results()
 
 # __________________________________________________________________________________________________________
 
 
-# def standarize_dataset(array_like_object):
-#     scaled_array = scaler.fit_transform(array_like_object)
-#     return scaled_array
-#
-#
-# def print_results(list_of_results):
-#     print('List of AUROC scores:', list_of_results)
-#     auroc_mean = np.mean(list_of_results)
-#     print('\nMean AUROC:', round((auroc_mean) * 100, 3), '%')
-#
-#
-# def train_model(model_class, df_x, df_y):
-#     model = model_class
-#     list_auroc_stratified = []
-#     array_x = df_x.to_numpy()
-#     array_y = df_y.to_numpy()
-#     for train_index, test_index in skf.split(df_x, df_y):
-#         x_train_fold, x_test_fold = array_x[train_index], array_x[test_index]
-#         y_train_fold, y_test_fold = array_y[train_index], array_y[test_index]
-#
-#         scaled_x_array_train_fold = standarize_dataset(x_train_fold)
-#         scaled_x_array_test_fold = standarize_dataset(x_test_fold)
-#
-#         model.fit(scaled_x_array_train_fold, np.ravel(y_train_fold))
-#         list_auroc_stratified.append(
-#             roc_auc_score(y_test_fold, model.predict_proba(scaled_x_array_test_fold)[:, 1]))
-#     print_results(list_auroc_stratified)
-
-# ------------------------------------------------------------------------------------------------
-
-# for tests
-df = pd.read_csv("Loan_data_new_variables.csv")
-myModel = Model(df.drop('target', axis=1), df[['target']])
-myModel.train_model(LogisticRegression(max_iter=500))
-compare_results('Logistic Regression', myModel.auroc_mean)
+# # for tests
+# df = pd.read_csv("Loan_data_new_variables.csv")
+# myModel = Model(df.drop('target', axis=1), df[['target']], LogisticRegression(max_iter=500))
+# myModel.train_model()
+# print(compare_results('Logistic Regression', myModel.auroc_mean))
